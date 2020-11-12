cfd453707ec41d238d51f7a5a6921089537381a5
Reselim <notreselim@gmail.com>
2020-11-12 04:37:20 -0500

    Replace Maid with ImmutableMaid

--- Packages/Util/Maid.lua
+++ Packages/Util/Maid.lua
@@ -1,43 +1,73 @@
-local TABLE_METHODS = {
-	"Disconnect", "disconnect",
-	"Destroy", "destroy"
+local LOCKED_ERROR = "Maid no longer usable, create a new maid if you wish to add events"
+
+local TABLE_SEARCH_FUNCTIONS = {
+	"Destroy", "destroy",
+	"Disconnect", "disconnect"
 }
+
+local function dispose(object)
+	local objectType = typeof(object)
+
+	if objectType == "function" then
+		object()
+	elseif objectType == "table" or objectType == "userdata" then
+		for _, functionName in ipairs(TABLE_SEARCH_FUNCTIONS) do
+			local disposeFunction = object[functionName]
+			if disposeFunction then
+				disposeFunction(object)
+				return
+			end
+		end
+	elseif objectType == "Instance" then
+		object:Destroy()
+	elseif objectType == "RBXScriptConnection" then
+		object:Disconnect()
+	end
+end
 
 local Maid = {}
 Maid.__index = Maid
 
-function Maid.new()
-	return setmetatable({
-		_tasks = {}
+function Maid.new(tasks)
+	local self = setmetatable({
+		_tasks = {},
+		_locked = false
 	}, Maid)
+
+	if tasks then
+		self:giveTasks(tasks)
+	end
+
+	return self
 end
 
-function Maid:GiveTask(task)
+function Maid:giveTask(task)
+	assert(not self._locked, LOCKED_ERROR)
 	table.insert(self._tasks, task)
 end
 
-function Maid:GiveTasks(tasks)
+function Maid:giveTasks(tasks)
+	assert(not self._locked, LOCKED_ERROR)
 	for _, task in pairs(tasks) do
-		self:GiveTask(task)
+		self:giveTask(task)
 	end
 end
 
-function Maid:Clean()
-	for _, task in pairs(self._tasks) do
-		if typeof(task) == "Instance" then
-			task:Destroy()
-		elseif typeof(task) == "RBXScriptConnection" then
-			task:Disconnect()
-		elseif typeof(task) == "table" then
-			for _, methodName in ipairs(TABLE_METHODS) do
-				if task[methodName] then
-					task[methodName](task)
-				end
-			end
+function Maid:clean()
+	-- Calling Maid:clean() multiple times is okay; it shouldn't error
+
+	-- Example case: Disconnecting any input listeners when a Roact component is unmounted
+	-- that may otherwise be disconnected when the input is ended.
+
+	if not self._locked then
+		self._locked = true
+
+		for _, task in pairs(self._tasks) do
+			dispose(task)
 		end
+
+		self._tasks = nil
 	end
 end
-
-Maid.Destroy = Maid.Clean
 
 return Maid
